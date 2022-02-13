1bb9e857b80f810c5d6254d581a6f95f87629212
DESKTOP-MNPCTGQ\José Mundarain <Jemundarain.15@est.ucab.edu.ve>
2022-02-13 11:04:51 -0400

    Adaptation to Olympic classification, counters and elimination of line breaks

--- scraper.py
+++ scraper.py
@@ -2,34 +2,25 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 import pandas as pd
+import array as arr
+import numpy as np
 
-# 1 Teoría de números
-# 2 Geometría
-# 3 Polinomios
-# 4 Desigualdades X
-# 5 Ecuaciones funcionales
-# 6 Combinatoriacls
-# 7 Juegos y estrategia
-# 8 Números y funciones
-# 9 Sucesiones y series
-
-# 1 - Aritmética 
-# 2 - Álgebra
-# 3 - Geometría 
-# 4 - Combinatoria 
-# 5 - Sucesiones y series 
-# 6 - Juegos de estrategia 
-# 7 - Polinomios y ecuaciones 
-# 8 - Lógica
-# 9 - Razonamiento en 2D
-# 10 - Razonamiento en 3D
+'''
+ID  CLASIFICACION OLIMPICA      CLASIFICACION JMM
+1   ÁLGEBRA                     3-Polinomios, 4-Desigualdades, 5-Ecuaciones funcionales, 9-Sucesiones y series
+2   GEOMETRÍA                   2-Geometría
+3   PROBABILIDAD                6-Combinatoria
+4   TEORÍA DE NÚMEROS           1-Teoría de números
+'''
 
 sn = 0
+count_prob = np.zeros((4,), dtype=int)
+
 with open('problems-training.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["SN", "Enunciado", "Category"])
     for i in range (1, 10):
-        if i!=4:
+        if i!=7 and i!=8:
             k = requests.get("https://wpd.ugr.es/~jmmanzano/preparacion/problemas.php?page=1&d=-1&c=" + str(i)).text
             soup = BeautifulSoup(k,'html.parser')
             page = int(int(soup.find("div", {"class": "paginador-boton-c"}).text.split()[-1][:-1])/5)
@@ -39,16 +30,19 @@ with open('problems-training.csv', 'w', newline='') as file:
                 for enunciado in soup.find_all("div", {"class": "enunciado"}):
                     sn = sn + 1
                     if i==1:
-                        writer.writerow([sn, enunciado.text, 1])
-                    if i==2:
-                        writer.writerow([sn, enunciado.text, 3])
-                    if i==3 or i==5:
-                        writer.writerow([sn, enunciado.text, 7])
-                    if i==6:
                         writer.writerow([sn, enunciado.text, 4])
-                    if i==7:    
-                        writer.writerow([sn, enunciado.text, 6])
-                    if i==8:
+                        count_prob[3] = count_prob[3] + 1
+                    if i==2:
                         writer.writerow([sn, enunciado.text, 2])
-                    if i==9:
-                        writer.writerow([sn, enunciado.text, 5])
+                        count_prob[1] = count_prob[1] + 1   
+                    if i==3 or i==4 or i==5 or i==9:
+                        writer.writerow([sn, enunciado.text, 1])
+                        count_prob[0] = count_prob[0] + 1
+                    if i==6:
+                        writer.writerow([sn, enunciado.text, 3])
+                        count_prob[2] = count_prob[2] + 1
+
+print("ÁLGEBRA  ->", count_prob[0], " problemas")
+print("GEOMETRÍA ->", count_prob[1], " problemas")
+print("PROBABILIDAD ->", count_prob[2], " problemas")
+print("TEORÍA DE NÚMEROS ->", count_prob[3], " problemas")
